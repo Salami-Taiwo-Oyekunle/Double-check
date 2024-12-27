d02dc9b7e4dab3acc44bc2e4d6ba390de64ee9e7
Salami-Taiwo-Oyekunle <salamitaiwo@yahoo.com>
2024-12-27 17:15:15 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n=int(input('Enter a number'))
+if n>50:
+    print('number is greater than 50')
+    if n%2==0:
+     print('number is even')
+    else:
+       print('number is odd')
+else:
+     print('number is less than 50')    
